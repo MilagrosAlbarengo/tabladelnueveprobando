1363c28c2a3ec6a90bea605b7e4f9f940e30afdb
MilagrosAlbarengo <92064009+MilagrosAlbarengo@users.noreply.github.com>
2021-10-30 21:28:37 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+let llamarfuncion: string = String(prompt("Ingrese A B C o D"));
+let resultado: number;
+resultado = 0;
+
+function tabladel(numerodetabla: number) {
+  for (let contador = 1; contador <= 9; contador++) {
+    llamarfuncion = string(prompt("Ingrese a,b,c,o d"));
+    resultado = contador * numerodetabla;
+    console.log(resultado);
+  }
+}
+
+switch (true) {
+  case llamarfuncion === a:
+    console.log(tabladel(9));
+    break;
+  case llamarfuncion === b:
+    console.log(tabladel(9));
+    break;
+  case llamarfuncion === c:
+    console.log(tabladel(9));
+    break;
+  case llamarfuncion === d:
+    console.log(tabladel(9));
+    break;
+  default:
+    break;
+}
